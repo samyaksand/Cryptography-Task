d921cd3a256d22be7d19f0a266d3501e13027b04
Samyak Sand <62803746+samyaksand@users.noreply.github.com>
2021-12-03 19:56:14 +0400

    Add dontsee.txt code

--- samyaksand.py
+++ samyaksand.py
@@ -55,40 +55,42 @@ for key in range(len(LETTERS)):
 # Encrpted Text - PDA JATP YELDAN GAUOMQWNA EO PDA WHLDWXAPO SEPDKQP F
 # Output - THE NEXT CIPHER KEYSQUARE IS THE ALPHABETS WITHOUT J
 
+print('\n------------------Decrypted Message found using key 22------------------')
+print('THE NEXT CIPHER KEYSQUARE IS THE ALPHABETS WITHOUT J')
 print('\n')
 
-# Caesar Cipher Decryption - Brute Force Way
+# Caesar Cipher Decryption using Key
+shift = 22 
+encrypted_text = "OPXWODPSKQLONCXQNUJEOLXPWAEHMOUZOEQXXVKUJOWBLMWXPQUIOELPMYKYEHMOGOKYQXAXKYKDLYQZYLYHAWWBLMWXQYLWVWOY"
+plain_text = ""
 
-message = 'OPXWODPSKQLONCXQNUJEOLXPWAEHMOUZOEQXXVKUJOWBLMWXPQUIOELPMYKYEHMOGOKYQXAXKYKDLYQZYLYHAWWBLMWXQYLWVWOY' 
-LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'  
+for c in encrypted_text:
+    if c.isupper():
+        c_unicode = ord(c)
+        c_index = ord(c) - ord("A")
 
-for key in range(len(LETTERS)):  
-        translated = ''  
-   
-        for symbol in message: 
-                if symbol in LETTERS:  
-                        n = LETTERS.find(symbol)  
-                        n = n - key  
-                        if n < 0:  
-                                n = n + len(LETTERS)  
-                        translated = translated + LETTERS[n]  
-         
-        
-                else:  
-                        translated = translated + symbol  
-      
-        print('Hacking k #%s: %s' % (key, translated))
+        new_index = (c_index - shift) % 26
+        new_unicode = new_index + ord("A")
 
-# Output Text -STBASHTWOUPSRGBURYNISPBTAEILQSYDSIUBBZOYNSAFPQABTUYMSIPTQCOCILQSKSOCUBEBOCOHPCUDCPCLEAAFPQABUCPAZASC
+        new_character = chr(new_unicode)
+
+        plain_text = plain_text + new_character
+
+    else:
+
+        plain_text = plain_text + c
+
+print("Encrypted Text:",encrypted_text)
+
+print("Decrypted Text:",plain_text)
 
 print('\n')
+
 
 from pycipher import Playfair
 print(Playfair('ABCDEFGHIKLMNOPQRSTUVWXYZ').decipher('STBASHTWOUPSRGBURYNISPBTAEILQSYDSIUBBZOYNSAFPQABTUYMSIPTQCOCILQSKSOCUBEBOCOHPCUDCPCLEAAFPQABUCPAZASC'))
 
 # Output - RSAENCRYPTNUMBERTWOHUNDREDFOURTYTHREEWITHNVALUEASTWOTHOUSANDFOURHUNDREDANDNINETEENANDEVALUEASELEVENX
-
-print('\n')
 
 print('Formatted Text - RSA ENCRYPT NUMBER TWO HUNDRED FOURTY THREE WITH N VALUE AS TWO THOUSAND FOUR HUNDRED AND NINETEEN AND E VALUE AS ELEVEN X')
 
@@ -109,3 +111,42 @@ print("Decrypted Message (using the formula m = C^d(mod N)): ", message)
 
 print('\n')
 
+
+# Caesar Cipher Conversion can be done using libraries
+# from pycipher import Caesar
+
+# print(Caesar(key=5).decipher('TM, DTZ KTZSI RJ. HTSLWFYX. YMNX NX YMJ JSILTFQ. TW NX NY?'))
+# print('\n')
+# Output - OHYOUFOUNDMECONGRATSTHISISTHEENDGOALORISIT
+
+print('--------------In dontsee.txt document-------------------\n')
+
+
+shift = 5 
+encrypted_text = "TM, DTZ KTZSI RJ. HTSLWFYX. YMNX NX YMJ JSILTFQ. TW NX NY?"
+plain_text = ""
+
+for c in encrypted_text:
+    if c.isupper():
+        c_unicode = ord(c)
+        c_index = ord(c) - ord("A")
+
+        new_index = (c_index - shift) % 26
+        new_unicode = new_index + ord("A")
+
+        new_character = chr(new_unicode)
+
+        plain_text = plain_text + new_character
+
+    else:
+
+        plain_text = plain_text + c
+
+print("Encrypted Text:",encrypted_text)
+
+print("Decrypted Text:",plain_text)
+
+
+print('\n')
+
+print('Decrypted Formatted Text : OH, YOU FOUND ME. CONGRATS. THIS IS THE ENDGOAL. OR IS IT?')
